5827f6e116461e161aaa27822dd583b10409f65b
lhsthree <lhsthree@gmail.com>
2021-11-30 23:27:05 -0600

    feat: import redux reducers

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-import store from "./redux/index"
-
+import store from "./redux"
 import {changeCount} from "./redux/count"
+import {addFavoriteThing, removeFavoriteThing} from "./redux/favoriteThings"
+import {setYouTubeTitle, incrementViewCount, upvoteVideo, downvoteVideo} from "./redux/youTubeVideo"
+
 
 store.dispatch(changeCount(42))
+store.dispatch(addFavoriteThing("Door bells"))
+store.dispatch(addFavoriteThing("Sleigh bells"))
+store.dispatch(removeFavoriteThing("door bells"))
+store.dispatch(setYouTubeTitle("Learning Redux is Fun!"))
+store.dispatch(incrementViewCount())
+store.dispatch(upvoteVideo())
+store.dispatch(incrementViewCount())
+store.dispatch(upvoteVideo())
+store.dispatch(incrementViewCount())
+store.dispatch(upvoteVideo())
+store.dispatch(downvoteVideo())
